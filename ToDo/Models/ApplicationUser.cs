83c351825da814f3921b18e717b11b65fcbb15ea
Kevin Penny <Kevin.penny3@yahoo.com>
2020-04-22 14:17:29 -0500

    added requireds and display names

--- ToDo/Models/ApplicationUser.cs
+++ ToDo/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,14 @@ namespace ToDo.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public Id MyProperty { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        public ApplicationUser ApplicationUser { get; set; }
-        public string ApplicationUser { get; set; }
+        public List<ToDoItem> ToDoItems { get; set; }
 
     }
 }
--- ToDo/Models/ToDoItem.cs
+++ ToDo/Models/ToDoItem.cs
@@ -1,6 +1,24 @@
-﻿namespace ToDo.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
 {
     public class ToDoItem
     {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Task Name")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name = "To Do Status")]
+        public bool ToDoStatus { get; set; }
+        public int ToDoStatusId  { get; set; }
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
     }
 }
